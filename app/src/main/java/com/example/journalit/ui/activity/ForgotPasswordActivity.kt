9d8c1dbd4edb87d72d79e4bd45f96766ab71fa01
rezen <rezen.khadgi@gmail.com>
2025-01-29 17:26:12 +0545

    login register connect with database

--- app/src/main/java/com/example/journalit/ui/activity/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/journalit/ui/activity/ForgotPasswordActivity.kt
@@ -1,21 +1,67 @@
 package com.example.journalit.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.journalit.R
+import com.example.journalit.databinding.ActivityForgotPasswordBinding
+import com.example.journalit.repository.UserRepositoryImpl
+import com.example.journalit.utils.LoadingUtils
+import com.example.journalit.viewmodel.UserViewModel
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    lateinit var binding: ActivityForgotPasswordBinding
+    lateinit var userViewModel: UserViewModel
+    lateinit var loadingUtils: LoadingUtils
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_forgot_password)
+
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadingUtils = LoadingUtils(this)
+
+        var repo = UserRepositoryImpl()
+        userViewModel = UserViewModel(repo)
+
+        binding.resetPasswordBtn.setOnClickListener {
+            loadingUtils.show()
+
+            var email: String =binding.emailForgotPassword.text.toString()
+
+            userViewModel.forgotPassword(email){
+                success, message ->
+                if(success){
+                    loadingUtils.dismiss()
+
+                    Toast.makeText(this@ForgotPasswordActivity,
+                        message, Toast.LENGTH_SHORT).show()
+                    finish()
+                }else{
+                    loadingUtils.dismiss()
+
+                    Toast.makeText(this@ForgotPasswordActivity,
+                        message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.signupForgotLink.setOnClickListener {
+            val intent = Intent(
+                this@ForgotPasswordActivity, LoginActivity::class.java
+            )
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/journalit/ui/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/journalit/ui/activity/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.journalit.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +11,13 @@ import com.example.journalit.R
 import com.example.journalit.databinding.ActivityRegisterBinding
 import com.example.journalit.model.UserModel
 import com.example.journalit.repository.UserRepositoryImpl
+import com.example.journalit.utils.LoadingUtils
 import com.example.journalit.viewmodel.UserViewModel
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
     lateinit var userViewModel: UserViewModel
+    lateinit var loadingUtils: LoadingUtils
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,13 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadingUtils = LoadingUtils(this)
         val repo = UserRepositoryImpl()
         userViewModel = UserViewModel(repo)
 
         binding.registerBtn.setOnClickListener{
+            loadingUtils.show()
+
             var username = binding.usernameRegister.text.toString()
             var email = binding.emailRegister.text.toString()
             var password = binding.passwordRegister.text.toString()
@@ -33,10 +39,12 @@ class RegisterActivity : AppCompatActivity() {
             userViewModel.register(email,password){
                 success,message,userId ->
                 if(success){
+                    loadingUtils.dismiss()
                     var userModel = UserModel(userId.toString(),
                         username,email)
                     addUser(userModel)
                 }else{
+                    loadingUtils.dismiss()
                     Toast.makeText(this@RegisterActivity,
                         message, Toast.LENGTH_SHORT).show()
                 }
@@ -47,6 +55,12 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.signinLink.setOnClickListener {
+            val intent = Intent(
+                this@RegisterActivity, LoginActivity::class.java
+            )
+            startActivity(intent)
         }
     }
 
