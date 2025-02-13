2ea7865ee896e48891aebd6d6d1c89e078648ce7
rezen <rezen.khadgi@gmail.com>
2025-02-13 18:05:09 +0545

    add story backend

--- app/src/main/java/com/example/journalit/repository/StoryRepositoryImpl.kt
+++ app/src/main/java/com/example/journalit/repository/StoryRepositoryImpl.kt
@@ -1,4 +1,159 @@
 package com.example.journalit.repository
 
-class StoryRepositoryImpl {
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import android.provider.OpenableColumns
+import com.cloudinary.Cloudinary
+import com.cloudinary.utils.ObjectUtils
+import com.example.journalit.model.StoryModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.io.InputStream
+import java.util.concurrent.Executors
+
+class StoryRepositoryImpl: StoryRepository {
+    val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    val ref: DatabaseReference = database.reference.child("stories")
+    override fun addStory(storyModel: StoryModel, callback: (Boolean, String) -> Unit) {
+
+        var id = ref.push().key.toString()
+        storyModel.storyId = id
+        ref.child(id).setValue(storyModel).addOnCompleteListener {
+            if(it.isSuccessful){
+                callback(true, "Story added successfully.")
+            }else{
+                callback(false, "${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun updateStory(
+        storyId: String,
+        data: MutableMap<String, Any>,
+        callback: (Boolean, String) -> Unit
+    ) {
+        ref.child(storyId).updateChildren(data).addOnCompleteListener {
+            if(it.isSuccessful){
+                callback(true, "Story updated successfully.")
+            }else{
+                callback(false, "${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun deleteStory(storyId: String, callback: (Boolean, String) -> Unit) {
+        ref.child(storyId).removeValue().addOnCompleteListener {
+            if(it.isSuccessful){
+                callback(true, "Story deleted successfully")
+            }else{
+                callback(false, "${it.exception?.message}")
+            }
+        }
+    }
+
+    override fun getStoryById(
+        storyId: String,
+        callback: (Boolean, String) -> Unit) {
+        ref.child(storyId).addValueEventListener(
+            object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()){
+                        var model = snapshot.getValue(StoryModel::class.java)
+                        callback(model,true,"Story fetched")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    callback(null,false,error.message)
+                }
+
+            }
+        )
+    }
+
+    override fun getAllStory(callback: (List<StoryModel>?, Boolean, String) -> Unit) {
+        ref.addValueEventListener(
+            object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    if (snapshot.exists()){
+                        var stories = mutableListOf<StoryModel>()
+
+                        for(eachData in snapshot.children){
+                            var model = eachData.getValue(StoryModel::class.java)
+                            if (model != null){
+                                stories.add(model)
+                            }
+                        }
+                        callback(stories,true,"Stories fetched")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    callback(null,false,error.message)
+                }
+
+            }
+        )
+    }
+
+    private val cloudinary = Cloudinary(
+        mapOf(
+            "cloud_name" to "dmnnqyjvj",
+            "api_key" to "719664345337452",
+            "api_secret" to "euvxoQyPG_ouM5nhLlZ3L6R5qIo"
+        )
+    )
+    override fun uploadImage(context: Context, imageUri: Uri, callback: (String?) -> Unit) {
+        val executor = Executors.newSingleThreadExecutor()
+        executor.execute {
+            try {
+                val inputStream: InputStream? = context.contentResolver.openInputStream(imageUri)
+                var fileName = getFileNameFromUri(context, imageUri)
+
+                fileName = fileName?.substringBeforeLast(".") ?: "uploaded_image"
+
+                val response = cloudinary.uploader().upload(
+                    inputStream, ObjectUtils.asMap(
+                        "public_id", fileName,
+                        "resource_type", "image"
+                    )
+                )
+
+                var imageUrl = response["url"] as String?
+                imageUrl = imageUrl?.replace("http://", "https://")
+
+                Handler(Looper.getMainLooper()).post {
+                    callback(imageUrl)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Handler(Looper.getMainLooper()).post {
+                    callback(null)
+                }
+            }
+        }
+    }
+
+    override fun getFileNameFromUri(context: Context, uri: Uri): String? {
+        var fileName: String? = null
+        val cursor: Cursor? = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val nameIndex = it.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                if (nameIndex != -1) {
+                    fileName = it.getString(nameIndex)
+                }
+            }
+        }
+        return fileName
+    }
+
 }
--- app/src/main/java/com/example/journalit/viewmodel/StoryViewModel.kt
+++ app/src/main/java/com/example/journalit/viewmodel/StoryViewModel.kt
@@ -1,4 +1,40 @@
 package com.example.journalit.viewmodel
 
+import android.content.Context
+import android.net.Uri
+import com.example.journalit.model.StoryModel
+
 class StoryViewModel {
+
+    fun addStory(storyModel: StoryModel,
+                 callback:(Boolean, String) -> Unit){
+    }
+
+    fun updateStory(storyId: String,
+                    data:MutableMap <String,Any>,
+                    callback: (Boolean, String) -> Unit){
+
+    }
+
+    fun deleteStory(storyId: String,
+                    callback: (Boolean, String) -> Unit){
+
+    }
+
+    fun getStoryById(storyId: String,
+                     callback: (Boolean, String) -> Unit){
+
+    }
+
+    fun getAllStory(callback: (List<StoryModel>?,
+                               Boolean, String) -> Unit){
+
+    }
+
+    fun uploadImage(context: Context, imageUri: Uri,
+                    callback: (String?) -> Unit){
+
+    }
+
+
 }
