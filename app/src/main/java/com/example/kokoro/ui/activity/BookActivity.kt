d546e2dce5282c5bf76c1e798fb7589aa33972a3
rezen <rezen.khadgi@gmail.com>
2025-02-28 01:14:32 +0545

    add book

--- app/src/main/java/com/example/kokoro/ui/activity/BookActivity.kt
+++ app/src/main/java/com/example/kokoro/ui/activity/BookActivity.kt
@@ -1,17 +1,39 @@
 package com.example.kokoro.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kokoro.R
+import com.example.kokoro.databinding.ActivityBookBinding
 
 class BookActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityBookBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_book)
+
+        binding = ActivityBookBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton3.setOnClickListener {
+            var intent = Intent(this@BookActivity,
+                AddBookActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+        binding.StoriesBtn.setOnClickListener {
+            val intent2 = Intent(this@BookActivity,
+                DashboardActivity::class.java)
+            startActivity(intent2)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
